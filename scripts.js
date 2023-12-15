47d4be207af08ae2de7844a91938f78651b767ec
Martin Krošlák <kroslakma@gmail.com>
2023-12-15 07:33:21 +0100

    Add form for gathering info about attendees

--- scripts.js
+++ scripts.js
@@ -1,5 +1,30 @@
 "use strict";
 
+function asyncRequest(xhr, body) {
+	return new Promise(function(resolve, reject) {
+		xhr.onload = function() {
+			if (this.status >= 200 && this.status < 300) {
+				resolve(xhr.response);
+			} else {
+				reject({
+					status: this.status,
+					statusText: xhr.statusText
+				});
+			}
+		};
+		xhr.onerror = function() {
+			reject({
+				status: this.status,
+				statusText: xhr.statusText
+			});
+		};
+		if (body)
+			xhr.send(body);
+		else
+			xhr.send();
+	});
+}
+
 const photos = {
 	innsbruck: {
 		title: "Innsbruck",
@@ -61,6 +86,49 @@ function onDocumentLoad(event) {
 			photo.addEventListener('click', onGalleryPhotoClick);
 		}
 	}
+
+	function onFormSubmit(event) {
+		event.preventDefault();
+		event.submitter.disabled = true;
+
+		const form = event.currentTarget;
+		const formData = new FormData(form);
+		const xhr = new XMLHttpRequest();
+		xhr.open('POST', form.action, true);
+
+		function asyncdone() {
+			event.submitter.disabled = false;
+		}
+		asyncRequest(xhr, formData).then(asyncdone, asyncdone);
+	}
+
+	const formSection = document.getElementById('form');
+	const form = formSection.children[1];
+
+	function refreshFormFields() {
+		const willAttend = form.elements.willAttend.value === 'true';
+		const wantAccomodation = form.elements.wantAccomodation.value === 'true';
+
+		form.elements.contactEmail.disabled = !willAttend;
+		form.elements.contactPhone.disabled = !willAttend;
+		form.elements.numAdults.disabled    = !willAttend;
+		form.elements.numChildren.disabled  = !willAttend;
+		form.elements.extra.disabled        = !willAttend;
+		for (const radioButton of form.elements.wantAccomodation)
+			radioButton.disabled = !willAttend;
+
+		form.elements.accomodationFrom.disabled = !willAttend || !wantAccomodation;
+		form.elements.numNights.disabled        = !willAttend || !wantAccomodation;
+	}
+
+	for (const radioButton of form.elements.willAttend)
+		radioButton.addEventListener('change', refreshFormFields);
+
+	for (const radioButton of form.elements.wantAccomodation)
+		radioButton.addEventListener('change', refreshFormFields);
+
+	refreshFormFields();
+	form.addEventListener('submit', onFormSubmit);
 }
 
 window.addEventListener('load', onDocumentLoad);
